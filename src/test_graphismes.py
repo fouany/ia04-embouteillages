87b5fe051b9911dfa06d9470659349502e0960d4
Alexandre Fournier <alexandre.fournier@etu.utc.fr>
2021-06-09 18:37:11 +0200

    Intégration graphismes simulation

--- src/test_graphismes.py
+++ src/test_graphismes.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+import json
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
@@ -11,7 +13,7 @@ y = np.sin(t)
 # Création de la figure et de l'axe
 fig, ax = plt.subplots()
 
-# Création de la ligne qui sera mise à jour au fur et à mesure
+# Création de la route
 line, = ax.plot(np.multiply(x, 0.93), np.multiply(y, 0.93), color='black')
 line2, = ax.plot(np.multiply(x, 1.07), np.multiply(y, 1.07), color='black')
 point, = ax.plot([], [], color="green",ls="none", marker="o")
@@ -22,15 +24,20 @@ point3, = ax.plot([], [], color="blue",ls="none", marker="o")
 ax.set_xlim([1.1*np.min(x), 1.1*np.max(x)])
 ax.set_ylim([1.1*np.min(y), 1.1*np.max(y)])
 
+f = open('car_positions.json',)
+car_positions = json.load(f)
+ps = car_positions["0"]
 
 # Création de la function qui sera appelée à "chaque nouvelle image"
 def animate(k):
-    i = min(k, x.size)
-    point.set_data(x[i], y[i])
-    point2.set_data(x[i - 5], y[i - 5])
-    return line, line2, point, point2
+    i = min(k, len(ps))
+    point.set_data(math.cos(car_positions["0"][i]), math.sin(car_positions["0"][i]))
+    point2.set_data(math.cos(car_positions["1"][i]), math.sin(car_positions["1"][i]))
+    point3.set_data(math.cos(car_positions["2"][i]), math.sin(car_positions["2"][i]))
+        
+    return line, line2, point, point2, point3
 
 # Génération de l'animation, frames précise les arguments numérique reçus par func (ici animate), 
 # interval est la durée d'une image en ms, blit gère la mise à jour
-ani = animation.FuncAnimation(fig=fig, func=animate, frames=range(x.size), interval=50, blit=True, repeat=True)
+ani = animation.FuncAnimation(fig=fig, func=animate, frames=range(len(ps)), interval=200, blit=True, repeat=True)
 plt.show()
